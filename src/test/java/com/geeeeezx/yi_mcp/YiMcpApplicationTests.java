f7b61da9f9c51044c8a6d01fca58df1ba4480bd9
geeeezx <geeeeezx0402@gmail.com>
2025-03-11 23:46:17 +0800

    init

---
+++ src/test/java/com/geeeeezx/yi_mcp/YiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeeeezx.yi_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
